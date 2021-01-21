48897ff5db94b4a8e8a9ee74d3335d9446c58604
Cory Redmond <code@cory.red>
2021-01-21 17:13:30 +0000

    Default to a file input
    - Clean up entrypoint.

--- cmd/rover_from_file/main.go
+++ cmd/rover_from_file/main.go
@@ -16,14 +16,19 @@ func main() {
 		verbose   bool
 	)
 
-	flag.StringVar(&inputFile, "input", "@std", `The input is the path to a file containing instructions.
+	flag.StringVar(&inputFile, "input", "input.txt", `The input is the path to a file containing instructions.
 Can be set to @std to read from the terminal input.`)
 	flag.BoolVar(&verbose, "verbose", false, "Output verbose logging.")
 	flag.Parse()
 
-	var reader io.Reader
-	var err error
-	if inputFile == "@std" {
+	var (
+		reader io.Reader
+		err    error
+		isStd  = inputFile == "@std"
+	)
+
+	// Set the reader.
+	if isStd {
 		reader = os.Stdin
 	} else {
 		reader, err = os.Open(inputFile)
@@ -35,21 +40,23 @@ Can be set to @std to read from the terminal input.`)
 
 	var executor = marsrover.NewExecutor(reader)
 
+	// Capture a ctrl+c signal to exit program
 	go func() {
 		sc := make(chan os.Signal, 1)
 		signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 		<-sc
-		fmt.Print("\n\n\n")
 
-		for _, v := range executor.Rovers {
-			fmt.Println(v.CurrentLocation)
-		}
+		complete(executor)
 		os.Exit(0)
 	}()
+	if isStd {
+		fmt.Println("Press Ctrl+C to complete and exit.")
+	}
 
+	// Application loop.
 	for err != marsrover.ErrorEndOfInstructions {
-
-		if inputFile == "@std" {
+		// Format console.
+		if isStd {
 			fmt.Print("> ")
 		}
 
@@ -57,5 +64,15 @@ Can be set to @std to read from the terminal input.`)
 		if err != nil && err != marsrover.ErrorEndOfInstructions {
 			_, _ = fmt.Fprintf(os.Stderr, "Tick: %s\n", err)
 		}
+	}
+
+	complete(executor)
+}
+
+// complete will output the locations of all rovers to stdout.
+func complete(executor marsrover.Executor) {
+	fmt.Print("\n----------\n")
+	for _, v := range executor.Rovers {
+		fmt.Println(v.CurrentLocation)
 	}
 }
